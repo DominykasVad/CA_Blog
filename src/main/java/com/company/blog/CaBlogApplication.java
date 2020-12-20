8d9d5bf0fbeb3ee12ceee92cee6a2385698f8c6d
Dominykas Vadeiša <domvadeisa@gmail.com>
2020-12-20 02:20:38 +0200

    Initial commit

---
+++ src/main/java/com/company/blog/CaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.company.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaBlogApplication.class, args);
+    }
+
+}
