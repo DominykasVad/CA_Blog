9fa1dc7f28cd5e279039cf9c1fac9c785d224bd0
Dominykas Vadeiša <domvadeisa@gmail.com>
2020-12-20 19:59:07 +0200

    Implemented getAllUsers method

--- src/main/java/com/company/blog/user/controller/UserController.java
+++ src/main/java/com/company/blog/user/controller/UserController.java
@@ -2,7 +2,12 @@ package com.company.blog.user.controller;
 
 import com.company.blog.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -16,4 +21,10 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getAllUsers(@PageableDefault(size = 10) Pageable pageable, Model model) {
+        model.addAttribute("usersPage", userService.getAllUsers(pageable));
+        return "user/user-list";
+    }
 }
