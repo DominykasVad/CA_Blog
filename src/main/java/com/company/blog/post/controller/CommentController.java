97cf8e68ddb1fab138842a851aa4848a161178cd
Dominykas Vadeiša <domvadeisa@gmail.com>
2020-12-20 20:14:46 +0200

    Implemented addComment method

--- src/main/java/com/company/blog/post/controller/CommentController.java
+++ src/main/java/com/company/blog/post/controller/CommentController.java
@@ -1,9 +1,14 @@
 package com.company.blog.post.controller;
 
+import com.company.blog.post.model.Comment;
 import com.company.blog.post.service.CommentService;
 import com.company.blog.post.service.PostService;
+import com.company.blog.user.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,5 +22,14 @@ public class CommentController {
     public CommentController(CommentService commentService, PostService postService) {
         this.commentService = commentService;
         this.postService = postService;
+    }
+
+    @PostMapping
+    public String addComment(@ModelAttribute("comment") Comment comment, @AuthenticationPrincipal User user) {
+        log.info("addComment({})", comment);
+        comment.setAuthor(user);
+        comment.setPost(postService.getPostById(comment.getPostId()));
+        Comment savedComment = commentService.addComment(comment);
+        return "redirect:/public/post/" + savedComment.getPost().getId();
     }
 }
