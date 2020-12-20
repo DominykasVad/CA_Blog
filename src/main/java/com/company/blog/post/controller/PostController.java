90f6985d89c6bd289e75976252d2da2f5b17aaa0
Dominykas Vadeiša <domvadeisa@gmail.com>
2020-12-20 20:05:16 +0200

    Implemented getAllPosts method

--- src/main/java/com/company/blog/post/controller/PostController.java
+++ src/main/java/com/company/blog/post/controller/PostController.java
@@ -3,7 +3,11 @@ package com.company.blog.post.controller;
 import com.company.blog.post.service.CommentService;
 import com.company.blog.post.service.PostService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,5 +21,11 @@ public class PostController {
     public PostController(PostService postService, CommentService commentService) {
         this.postService = postService;
         this.commentService = commentService;
+    }
+
+    @GetMapping
+    public String getAllPosts(@PageableDefault(size = 9) Pageable pageable, Model model) {
+        model.addAttribute("postPage", postService.getAllPosts(pageable));
+        return "post/post-list";
     }
 }
