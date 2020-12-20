477d741d8bb933b86712d1ba786b237c011b6aad
Dominykas Vadeiša <domvadeisa@gmail.com>
2020-12-20 20:03:42 +0200

    Added PostController

---
+++ src/main/java/com/company/blog/post/controller/PostController.java
@@ -0,0 +1,21 @@
+package com.company.blog.post.controller;
+
+import com.company.blog.post.service.CommentService;
+import com.company.blog.post.service.PostService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("public/post")
+public class PostController {
+
+    private PostService postService;
+    private CommentService commentService;
+
+    public PostController(PostService postService, CommentService commentService) {
+        this.postService = postService;
+        this.commentService = commentService;
+    }
+}
